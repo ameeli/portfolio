02ced85cec0390aafd6d7dacb7c243a43572b59d
Amee Li <ameejli@gmail.com>
2025-09-11 10:34:49 -0700

    Use Tailwind colors

--- src/app/components/Motion.tsx
+++ src/app/components/Motion.tsx
@@ -2,13 +2,6 @@
 import { useRef } from "react";
 import { motion } from "motion/react";
 
-const box = {
-  width: 100,
-  height: 100,
-  backgroundColor: "#dd00ee",
-  borderRadius: 10,
-};
-
 export default function Motion() {
   const constraintsRef = useRef<HTMLDivElement>(null);
 
@@ -16,11 +9,11 @@ export default function Motion() {
     <div ref={constraintsRef}>
       <motion.div
         drag
-        style={box}
+        animate={{ rotate: 360 }}
+        className="rounded-md h-25 w-25 bg-primary"
         dragConstraints={constraintsRef}
         dragElastic={0.5}
         whileDrag={{ scale: 1.2, backgroundColor: "#f00" }}
-        animate={{ rotate: 360 }}
       />
     </div>
   );
