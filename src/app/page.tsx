41de1ccdd6ac35149143a77fab001cc862478fa9
Amee Li <ameejli@gmail.com>
2025-09-24 13:53:29 -0700

    Flex spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Welcome from "./pages/Welcome";
 
 export default function Home() {
   return (
-    <div className="p-15">
+    <div className="min-h-screen flex flex-col p-15">
       <Welcome />
-      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+      {/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div> */}
       {/* <CanvasSketch /> */}
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
+      {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"> */}
+      {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-secondary text-foreground gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -26,7 +26,7 @@ export default function Home() {
             Read our docs
           </a>
         </div> */}
-      </main>
+      {/* </main> */}
       {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer> */}
     </div>
   );
--- src/app/pages/Welcome.tsx
+++ src/app/pages/Welcome.tsx
@@ -2,10 +2,16 @@ import WordPath from "../components/WordPath";
 
 export default function Welcome() {
   return (
-    <div>
-      <h1 className="header">Hallo! I&apos;m Amee.</h1>
-      <h3 className="subheader">Welcome to my playground of learning and</h3>
-      <WordPath />
-    </div>
+    <>
+      <div className="flex-[3]">
+        <WordPath />
+      </div>
+      <div className="flex-[1]">
+        <h1 className="header">Hallo! I&apos;m Amee.</h1>
+        <h3 className="subheader">
+          Welcome to my playground of learning and exploration.
+        </h3>
+      </div>
+    </>
   );
 }
