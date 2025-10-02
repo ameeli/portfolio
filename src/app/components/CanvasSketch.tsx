c7992b711f56993ff06ace7153ea6f167a7d79a2
Amee Li <ameejli@gmail.com>
2025-10-02 15:13:04 -0700

    Unedited recursive tree

--- src/app/components/CanvasSketch.tsx
+++ src/app/components/CanvasSketch.tsx
@@ -12,14 +12,53 @@ export default function CanvasSketch() {
       const p5 = p5Module.default;
 
       const sketch = (p: P5) => {
+        let angle: number;
+
         p.setup = () => {
-          p.createCanvas(600, 400, p.WEBGL);
+          p.createCanvas(710, 400);
+          p.colorMode(p.HSB);
+          p.angleMode(p.DEGREES);
         };
 
         p.draw = () => {
-          p.background(240);
-          p.rotateY(p.frameCount * 0.01);
-          p.box(100);
+          p.background(0);
+          angle = (p.mouseX / p.width) * 90;
+          angle = p.min(angle, 90);
+
+          p.push();
+          p.translate(p.width / 2, p.height);
+
+          p.stroke(0, 255, 255);
+          p.line(0, 0, 0, -120);
+
+          p.translate(0, -120);
+
+          branch(120, 0);
+          p.pop();
+        };
+
+        const branch = (h: number, level: number) => {
+          p.stroke(level * 25, 255, 255);
+
+          h *= 0.66;
+
+          if (h > 2) {
+            // Draw the right branch
+            p.push();
+            p.rotate(angle);
+            p.line(0, 0, 0, -h);
+            p.translate(0, -h);
+            branch(h, level + 1);
+            p.pop();
+
+            // Draw the left branch
+            p.push();
+            p.rotate(-angle);
+            p.line(0, 0, 0, -h);
+            p.translate(0, -h);
+            branch(h, level + 1);
+            p.pop();
+          }
         };
       };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Welcome from "./components/Welcome";
 import StarPath from "./components/StarPath";
+import CanvasSketch from "./components/CanvasSketch";
 
 export default function Home() {
   return (
     <div className="min-h-screen p-15">
       <section aria-label="Star animation">
         <StarPath />
+      </section>
+      <section aria-label="Recursive tree">
+        <CanvasSketch />
       </section>
       <section aria-label="Welcome message">
         <Welcome />
